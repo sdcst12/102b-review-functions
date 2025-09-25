a52379b206cde7b9ed91a7c631d4efebad93907e
hungrybeagle <cyang@deltalearns.ca>
2025-09-25 08:11:02 -0700

    Update sample2.py

--- sample2.py
+++ sample2.py
@@ -23,3 +23,12 @@ print(f"x is {listVal[0]} and y is {listVal[1]}. y is a type { type(listVal)} va
 # this code throws an error because it receives 2 values but wants to receive 3
 x,y,z = getRandomXY()
 print(x)
+
+# sometimes we only want to capture one of the return values
+# one way to do this is to use an underscore for the value we want to ignore
+x,_ = getRandomXY()
+print(x)
+
+#another way is to just access the value by index
+x = getRandomXY()[0]
+print(x)
